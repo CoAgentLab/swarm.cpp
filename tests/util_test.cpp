b7a718a05481fd47dad7fb8725f317e2d7131f65
Coldmooon <howtoliy@gmail.com>
2024-10-25 15:14:39 +0800

    updated unit test content

--- tests/util_test.cpp
+++ tests/util_test.cpp
@@ -49,6 +49,194 @@ TEST(UtilTest, FunctionToJson) {
     EXPECT_EQ(json_output["function"]["parameters"]["required"], nlohmann::json::array({"param1"}));
 }
 
+
+//Test class Agent in types.h
+TEST(AgentTest, AgentConstructor) {
+    Agent agent;
+    EXPECT_EQ(agent.name, "Agent");
+    EXPECT_EQ(agent.model, "gpt-4o");
+    EXPECT_EQ(agent.instructions, "You are a helpful agent.");
+    EXPECT_EQ(agent.functions.size(), 0);
+    EXPECT_EQ(agent.tool_choice, "");
+    EXPECT_TRUE(agent.parallel_tool_calls);
+}
+
+TEST(AgentTest, AgentCopyConstructor) {
+    Agent agent;
+    Agent copy_agent(agent);
+    EXPECT_EQ(copy_agent.name, "Agent");
+    EXPECT_EQ(copy_agent.model, "gpt-4o");
+    EXPECT_EQ(copy_agent.instructions, "You are a helpful agent.");
+    EXPECT_EQ(copy_agent.functions.size(), 0);
+    EXPECT_EQ(copy_agent.tool_choice, "");
+    EXPECT_TRUE(copy_agent.parallel_tool_calls);
+}
+
+TEST(AgentTest, AgentMoveConstructor) {
+    Agent agent;
+    Agent copy_agent(std::move(agent));
+    EXPECT_EQ(copy_agent.name, "Agent");
+    EXPECT_EQ(copy_agent.model, "gpt-4o");
+    EXPECT_EQ(copy_agent.instructions, "You are a helpful agent.");
+    EXPECT_EQ(copy_agent.functions.size(), 0);
+    EXPECT_EQ(copy_agent.tool_choice, "");
+    EXPECT_TRUE(copy_agent.parallel_tool_calls);
+}
+
+TEST(AgentTest, AgentAssignment) {
+    Agent agent;
+    Agent copy_agent;
+    copy_agent = agent;
+    EXPECT_EQ(copy_agent.name, "Agent");
+    EXPECT_EQ(copy_agent.model, "gpt-4o");
+    EXPECT_EQ(copy_agent.instructions, "You are a helpful agent.");
+    EXPECT_EQ(copy_agent.functions.size(), 0);
+    EXPECT_EQ(copy_agent.tool_choice, "");
+    EXPECT_TRUE(copy_agent.parallel_tool_calls);
+}
+
+TEST(AgentTest, AgentMoveAssignment) {
+    Agent agent;
+    Agent copy_agent;
+    copy_agent = std::move(agent);
+    EXPECT_EQ(copy_agent.name, "Agent");
+    EXPECT_EQ(copy_agent.model, "gpt-4o");
+    EXPECT_EQ(copy_agent.instructions, "You are a helpful agent.");
+    EXPECT_EQ(copy_agent.functions.size(), 0);
+    EXPECT_EQ(copy_agent.tool_choice, "");
+    EXPECT_TRUE(copy_agent.parallel_tool_calls);
+}
+
+TEST(AgentTest, Agentassignment) {
+    Agent agent;
+    agent.set_name("Test Agent");
+    EXPECT_EQ(agent.get_name(), "Test Agent");
+
+    agent.set_model("chatgpt o1");
+    EXPECT_EQ(agent.get_model(), "chatgpt o1");
+
+    agent.set_instructions("You are a test agent.");
+    EXPECT_EQ(agent.get_instructions(), "You are a test agent.");
+}
+
+
+// Test Response class in types.h
+TEST(ResponseTest, ResponseConstructor) {
+    Response response;
+    EXPECT_EQ(response.messages.size(), 0);
+    EXPECT_EQ(response.agent, nullptr);
+    EXPECT_EQ(response.context_variables.size(), 0);
+}
+
+TEST(ResponseTest, ResponseCopyConstructor) {
+    Response response;
+    Response copy_response(response);
+    EXPECT_EQ(copy_response.messages.size(), 0);
+    EXPECT_EQ(copy_response.agent, nullptr);
+    EXPECT_EQ(copy_response.context_variables.size(), 0);
+}
+
+TEST(ResponseTest, ResponseMoveConstructor) {
+    Response response;
+    Response copy_response(std::move(response));
+    EXPECT_EQ(copy_response.messages.size(), 0);
+    EXPECT_EQ(copy_response.agent, nullptr);
+    EXPECT_EQ(copy_response.context_variables.size(), 0);
+}
+
+TEST(ResponseTest, ResponseAssignment) {
+    Response response;
+    Response copy_response;
+    copy_response = response;
+    EXPECT_EQ(copy_response.messages.size(), 0);
+    EXPECT_EQ(copy_response.agent, nullptr);
+    EXPECT_EQ(copy_response.context_variables.size(), 0);
+}
+
+TEST(ResponseTest, ResponseMoveAssignment) {
+    Response response;
+    Response copy_response;
+    copy_response = std::move(response);
+    EXPECT_EQ(copy_response.messages.size(), 0);
+    EXPECT_EQ(copy_response.agent, nullptr);
+    EXPECT_EQ(copy_response.context_variables.size(), 0);
+}
+
+
+TEST(ResponseTest, ResponseModify) {
+    Response response;
+    response.add_message({{"Messagekey", "Messagevalue"}});
+    EXPECT_EQ(response.messages.size(), 1);
+    EXPECT_EQ(response.messages[0]["Messagekey"], "Messagevalue");
+
+    response.set_agent(std::make_shared<Agent>());
+    EXPECT_EQ(response.agent->name, "Agent");
+    EXPECT_EQ(response.agent->model, "gpt-4o");
+    EXPECT_EQ(response.agent->instructions, "You are a helpful agent.");
+
+    response.set_context({{"Contextkey", "Contextvalue"}});
+    EXPECT_EQ(response.context_variables["Contextkey"], "Contextvalue");
+}
+
+
+// Test Result class in types.h
+TEST(ResultTest, ResultConstructor) {
+    Result result;
+    EXPECT_EQ(result.value, "");
+    EXPECT_EQ(result.agent, nullptr);
+    EXPECT_EQ(result.context_variables.size(), 0);
+}
+
+TEST(ResultTest, ResultCopyConstructor) {
+    Result result;
+    Result copy_result(result);
+    EXPECT_EQ(copy_result.value, "");
+    EXPECT_EQ(copy_result.agent, nullptr);
+    EXPECT_EQ(copy_result.context_variables.size(), 0);
+}
+
+TEST(ResultTest, ResultMoveConstructor) {
+    Result result;
+    Result copy_result(std::move(result));
+    EXPECT_EQ(copy_result.value, "");
+    EXPECT_EQ(copy_result.agent, nullptr);
+    EXPECT_EQ(copy_result.context_variables.size(), 0);
+}
+
+TEST(ResultTest, ResultCopyAssignment) {
+    Result result;
+    Result copy_result;
+    copy_result = result;
+    EXPECT_EQ(copy_result.value, "");
+    EXPECT_EQ(copy_result.agent, nullptr);
+    EXPECT_EQ(copy_result.context_variables.size(), 0);
+}
+
+TEST(ResultTest, ResultMoveAssignment) {
+    Result result;
+    Result copy_result;
+    copy_result = std::move(result);
+    EXPECT_EQ(copy_result.value, "");
+    EXPECT_EQ(copy_result.agent, nullptr);
+    EXPECT_EQ(copy_result.context_variables.size(), 0);
+}
+
+TEST(ResultTest, ResultModify) {
+    Result result;
+
+    result.set_value("Test value");
+    EXPECT_EQ(result.value, "Test value");
+
+    result.set_agent(std::make_shared<Agent>());
+    EXPECT_EQ(result.agent->name, "Agent");
+    EXPECT_EQ(result.agent->model, "gpt-4o");
+    EXPECT_EQ(result.agent->instructions, "You are a helpful agent.");
+
+    result.set_context({{"Contextkey", "Contextvalue"}});
+    EXPECT_EQ(result.context_variables["Contextkey"], "Contextvalue");
+}
+
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
